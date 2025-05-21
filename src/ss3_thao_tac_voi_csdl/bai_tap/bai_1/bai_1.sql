c2102e7f7af946370385bcb2911dc304d83739f7
Vinhnd03 <ducvinhmycanh@gmail.com>
2025-05-21 13:36:10 +0700

    nop bai tap ss3

---
+++ src/ss3_thao_tac_voi_csdl/bai_tap/bai_1/bai_1.sql
@@ -0,0 +1,85 @@
+create database quan_ly_sinh_vien3;
+
+use quan_ly_sinh_vien3;
+
+create table classes(
+	id int auto_increment primary key,
+    name varchar(20),
+    start_date date,
+    status boolean
+);
+
+create table students(
+	id int auto_increment primary key,
+    name varchar(50),
+    address varchar(50),
+    phone varchar(11),
+    status boolean,
+    class_id int,
+    foreign key(class_id) references classes(id)
+);
+
+create table subjects(
+	id int auto_increment primary key,
+    name varchar(20),
+    credit int,
+    status boolean
+);
+
+create table marks(
+	id int auto_increment primary key,
+    subject_id int,
+    student_id int,
+    mark int,
+    exam_times int,
+    foreign key(subject_id) references subjects(id),
+    foreign key(student_id) references students(id)
+);
+
+INSERT INTO classes
+VALUES 
+(1, 'A1', '2008-12-20', 1),
+(2, 'A2', '2008-12-22', 1),
+(3, 'B3', current_date, 0);
+
+INSERT INTO students (name, address, phone, status, class_id)
+VALUES 
+('Hung', 'Ha Noi', '0912113113', 1, 1),
+('Hoa', 'Hai phong',NULL, 1, 1),
+('Manh', 'HCM', '0123123123', 0, 2);
+
+INSERT INTO subjects (name, credit, status)
+VALUES 
+('CF', 5, 1),
+ ('C', 6, 1),
+ ('HDJ', 5, 1),
+ ('RDBMS', 10, 1);
+ 
+ INSERT INTO marks (subject_id, student_id, mark, exam_times)
+VALUES (1, 1, 8, 1),
+ (1, 2, 10, 2),
+ (2, 1, 12, 1);
+
+
+select s.id, s.name, c.name from students s join classes c on s.class_id = c.id
+where c.name = 'A1';
+
+select s.id, s.name, sub.name, m.mark from students s join marks m on s.id = m.student_id join subjects sub on m.subject_id 
+where sub.name = 'CF';
+
+-- Hiển thị tất cả các sinh viên có tên bắt đầu bảng ký tự ‘h’
+select * from students where name like 'h%';
+
+-- Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12.
+select * from classes where month(start_date) = 12;
+
+-- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5.
+select * from subjects where credit >= 3 and credit <= 5;
+
+-- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2.
+update students set class_id = 2 where name = 'Hung';
+
+-- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
+select s.id, s.name, sub.name, m.mark from students s join marks m on s.id = m.student_id join subjects sub on m.subject_id
+group by s.name, sub.name
+order by m.mark desc, s.name asc;
---
+++ src/ss3_thao_tac_voi_csdl/bai_tap/bai_2/bai_2.sql
@@ -0,0 +1,80 @@
+create database quan_ly_ban_hang2;
+
+use quan_ly_ban_hang2;
+
+CREATE TABLE Customer (
+    cID INT PRIMARY KEY,
+    Name VARCHAR(25),
+    cAge TINYINT
+);
+
+INSERT INTO Customer (cID, Name, cAge) VALUES
+(1, 'Minh Quan', 10),
+(2, 'Ngoc Oanh', 20),
+(3, 'Hong Ha', 50);
+
+CREATE TABLE `Order` (
+    oID INT PRIMARY KEY,
+    cID INT,
+    oDate DATETIME,
+    oTotalPrice INT,
+    FOREIGN KEY (cID) REFERENCES Customer(cID)
+);
+
+INSERT INTO `Order` (oID, cID, oDate, oTotalPrice) VALUES
+(1, 1, '2006-03-21', NULL),
+(2, 2, '2006-03-23', NULL),
+(3, 1, '2006-03-16', NULL);
+
+CREATE TABLE Product (
+    pID INT PRIMARY KEY,
+    pName VARCHAR(25),
+    pPrice INT
+);
+
+INSERT INTO Product (pID, pName, pPrice) VALUES
+(1, 'May Giat', 3),
+(2, 'Tu Lanh', 5),
+(3, 'Dieu Hoa', 7),
+(4, 'Quat', 1),
+(5, 'Bep Dien', 2);
+
+CREATE TABLE OrderDetail (
+    oID INT,
+    pID INT,
+    odQTY INT,
+    FOREIGN KEY (oID) REFERENCES `Order`(oID),
+    FOREIGN KEY (pID) REFERENCES Product(pID)
+);
+
+INSERT INTO OrderDetail (oID, pID, odQTY) VALUES
+(1, 1, 3),
+(1, 3, 7),
+(1, 4, 2),
+(2, 5, 4),
+(3, 2, 5),
+(3, 3, 3);
+
+-- Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
+select * from `Order`;
+
+-- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
+select A.*, D.* from Customer A 
+join `Order` B on A.cID = b.cID 
+join OrderDetail C on B.oID = C.oID
+join Product D on C.pID = D.pID
+group by a.cId, d.pID;
+
+-- Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào
+select A.* from Customer A 
+left join `Order` B on A.cID = b.cID 
+where B.oId IS NULL;
+
+-- Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn (giá một hóa đơn được tính bằng tổng giá bán
+-- của từng loại mặt hàng xuất hiện trong hóa đơn. Giá bán của từng loại được tính = odQTY*pPrice)
+select A.oID, A.oDate, sum(B.odQTY * C.pPrice) as 'totalPRice' from `Order` A
+join OrderDetail B on A.oID = B.oId
+join Product C on B.pID = C.pID
+group by A.oID, A.oDate
+
+
