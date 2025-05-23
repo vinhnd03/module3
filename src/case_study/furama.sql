6b2f8accc44d0f836de08fd1580ad1a7d4c1629f
Vinhnd03 <ducvinhmycanh@gmail.com>
2025-05-23 10:10:36 +0700

    nop bai tap ss5

--- src/case_study/furama.sql
+++ src/case_study/furama.sql
@@ -301,11 +301,87 @@ WHERE h.ma_hop_dong IS NULL;
 
 -- 7. Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các
 -- loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
-select * from dich_vu d 
+select d.* from dich_vu d 
 join loai_dich_vu l on d.ma_loai_dich_vu = l.ma_loai_dich_vu
 where d.ma_dich_vu in (
 	select ma_dich_vu from hop_dong where year(ngay_lam_hop_dong) = 2020
 ) and d.ma_dich_vu not in (
 	select ma_dich_vu from hop_dong where year(ngay_lam_hop_dong) = 2021
 );
+
+-- 8. Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+-- Học viên sử dụng theo 3 cách khác nhau để thực hiện yêu cầu trên.
+select distinct kh.ho_ten from khach_hang kh;
+
+select kh.ho_ten from khach_hang kh 
+group by kh.ho_ten;
+
+-- 9.Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng 
+-- trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+select month(hd.ngay_lam_hop_dong) as thang, count(hd.ma_khach_hang) as so_luong_khach_hang from hop_dong hd
+where year(hd.ngay_lam_hop_dong) = 2021 group by thang order by thang;
+
+-- 10. Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm.
+-- Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem
+-- (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+select hd.ma_hop_dong, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, hd.tien_dat_coc, 
+ifnull(sum(hdct.so_luong), 0) as so_luong_dich_vu_di_kem from hop_dong hd
+left join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+left join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+group by hd.ma_hop_dong;
+
+-- 11. Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng 
+-- có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
+
+select dvdk.* from dich_vu_di_kem dvdk
+join hop_dong_chi_tiet hdct on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+join hop_dong hd on hdct.ma_hop_dong = hd.ma_hop_dong
+join khach_hang kh on hd.ma_khach_hang = kh.ma_khach_hang
+join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+where lk.ten_loai_khach = "Diamond" and (kh.dia_chi like "% Vinh" or kh.dia_chi like "% Quảng Ngãi")
+group by dvdk.ma_dich_vu_di_kem;
+
+-- 12. Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng),
+--  ten_dich_vu, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem),
+--  tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020
+--  nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
+select 
+hd.ma_hop_dong, nv.ho_ten as ho_ten_nhan_vien, kh.ho_ten as ho_ten_khach_hang, kh.so_dien_thoai, dv.ten_dich_vu,
+ifnull(sum(hdct.so_luong), 0)  as so_luong_dich_vu_di_kem, hd.tien_dat_coc
+ from hop_dong hd
+join nhan_vien nv on hd.ma_nhan_vien = nv.ma_nhan_vien
+join khach_hang kh on hd.ma_khach_hang = kh.ma_khach_hang
+join dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
+left join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+where (year(hd.ngay_lam_hop_dong) = 2020 and month(hd.ngay_lam_hop_dong) in (10,11,12))
+and not (year(hd.ngay_lam_hop_dong) = 2021 and month(hd.ngay_lam_hop_dong) between 1 and 6)
+group by hd.ma_hop_dong;
+
+-- 13. Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng.
+-- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau
+select dvdk.ma_dich_vu_di_kem, dvdk.ten_dich_vu_di_kem, sum(hdct.so_luong) so_luong_dich_vu_di_kem
+ from dich_vu_di_kem dvdk 
+join hop_dong_chi_tiet hdct on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+where dvdk.ma_dich_vu_di_kem in 
+(select ma_dich_vu_di_kem from hop_dong_chi_tiet group by ma_dich_vu_di_kem having sum(so_luong) = 
+(select sum(so_luong) from hop_dong_chi_tiet group by ma_hop_dong_chi_tiet order by sum(so_luong) desc limit 1))
+group by dvdk.ma_dich_vu_di_kem;
+
+-- 14. Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. 
+-- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung 
+-- (được tính dựa trên việc count các ma_dich_vu_di_kem).
+
+select hd.ma_hop_dong, ldv.ten_loai_dich_vu, dvdk.ten_dich_vu_di_kem, count(dvdk.ma_dich_vu_di_kem) as so_lan_su_dung
+from hop_dong hd
+join dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
+join loai_dich_vu ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+group by dvdk.ten_dich_vu_di_kem
+having so_lan_su_dung = 1;
+
+-- 15. Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan,
+--  so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+
+
 
---
+++ src/ss5_view_index_sp_function_trigger/bai_tap/bai_tap1.sql
@@ -0,0 +1,105 @@
+create database demo_product;
+
+use demo_product;
+
+create table Products (
+	id int primary key auto_increment,
+    productCode varchar(50) not null,
+    productName varchar(100),
+    productPrice decimal(10,2),
+    productAmount int,
+    productDescription varchar(255),
+    productStatus varchar(20)
+);
+
+insert into Products (productCode, productName, productPrice, productAmount, productDescription, productStatus) values
+('P001', 'Keyboard', 150.00, 20, 'Mechanical keyboard', 'Available'),
+('P002', 'Mouse', 50.00, 50, 'Wireless mouse', 'Available'),
+('P003', 'Monitor', 300.00, 15, '24 inch HD monitor', 'Out of Stock'),
+('P004', 'Laptop', 1200.00, 10, 'Gaming laptop', 'Available'),
+('P005', 'Webcam', 80.00, 30, 'HD webcam', 'Available');
+
+-- Tạo Unique Index trên bảng Products (sử dụng cột productCode để tạo chỉ mục)
+	create unique index p_index on Products(productCode);
+    -- drop index p_index on Products;
+
+-- Tạo Composite Index trên bảng Products (sử dụng 2 cột productName và productPrice)
+	create index p_index2 on Products(productName, productPrice);
+
+-- Sử dụng câu lệnh EXPLAIN để biết được câu lệnh SQL của bạn thực thi như nào
+	explain select productCode from Products;
+    explain select productName from Products;
+
+-- So sánh câu truy vấn trước và sau khi tạo index
+	explain select productAmount from Products;
+    
+--  Tạo view lấy về các thông tin: productCode, productName, productPrice, productStatus từ bảng products.
+	create view p_view as (
+		select productCode, productName, productPrice, productStatus from Products
+    );
+	select * from p_view;
+    
+-- Tiến hành sửa đổi view
+	alter view p_view as (
+		select productCode, productName, productPrice from Products
+    );
+
+-- Tiến hành xoá view
+	drop view p_view;
+    
+-- Tạo store procedure lấy tất cả thông tin của tất cả các sản phẩm trong bảng product
+	Delimiter //
+    create procedure product_sp ()
+    begin
+		select * from Products;
+    end//
+    Delimiter ;
+    
+    call product_sp();
+
+-- Tạo store procedure thêm một sản phẩm mới
+Delimiter //
+    create procedure create_product_sp (
+		    p_code varchar(50),
+			p_name varchar(100),
+			p_price decimal(10,2),
+			p_amount int,
+			p_description varchar(255),
+			p_status varchar(20)
+    )
+    begin
+		insert into Products (productCode, productName, productPrice, productAmount, productDescription, productStatus) values
+        (p_code, p_name, p_price, p_amount, p_description, p_status);
+    end//
+    Delimiter ;
+
+-- Tạo store procedure sửa thông tin sản phẩm theo id
+	Delimiter //
+    create procedure update_product_by_id_sp (IN
+			id_product int,
+            p_code varchar(50),
+			p_name varchar(100),
+			p_price decimal(10,2),
+			p_amount int,
+			p_description varchar(255),
+			p_status varchar(20)
+    )
+    begin
+		update Products set productCode = p_code,
+        productName = p_name, 
+        productPrice = p_price, 
+        productAmount = p_amount,
+        productDescription = p_description, 
+        productStatus = p_status where id = id_product;
+    end//
+    Delimiter ;
+
+-- Tạo store procedure xoá sản phẩm theo id
+Delimiter //
+    create procedure delete_product_by_id_sp (IN
+			id_product int
+    )
+    begin
+		delete from Products where id = id_product;
+    end//
+    Delimiter ;
